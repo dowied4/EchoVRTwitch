443356312f73afc31f176718643fcb12eafb0fea
dowie <dowied4@mymacewan.ca>
2019-10-31 14:35:46 -0600

    Adds stats to player cards, fixes blue team not showing up
    Needed to fix the blue team card not showing up.

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react'
+import { Card, Segment, Grid } from 'semantic-ui-react'
 
 class PlayerCard extends Component {
     constructor(props) {
@@ -7,21 +7,25 @@ class PlayerCard extends Component {
         this.state = {
             loaded: false
         }
+        this.buildStats = this.buildStats.bind(this)
     }
 
     componentDidMount(){
         if (this.props.player && this.props.mouseInfo){
+            var playerMod = this.props.player;
+            playerMod.stats.possession_time = Math.round(this.props.player.stats.possession_time)
             this.setState({
                 player: this.props.player,
                 mouse: this.props.mouseInfo,
+                isOrange: this.props.isOrange,
                 loaded: true
             })
         }
     }
 
     componentDidUpdate(prevProps, prevState){
-        console.log("Prev state: ", prevState)
-        console.log("Current state: ", this.state)
+        // console.log("Prev state: ", prevState)
+        // console.log("Current state: ", this.state)
         if(prevProps.mouseInfo !== this.props.mouseInfo) {
             this.setState({
                 mouse: this.props.mouseInfo
@@ -29,19 +33,70 @@ class PlayerCard extends Component {
         }
     }
 
+    buildStats(){
+        return(
+            <Grid centered style={{marginLeft: 2}}>
+                <Grid.Row columns={4}>
+                    <Grid.Column ><h5>Assists:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.assists}</Grid.Column>
+                    <Grid.Column><h5>Blocks:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.blocks}</Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={4}>
+                    <Grid.Column><h5>Catches:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.catches}</Grid.Column>
+                    <Grid.Column><h5>Goals:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.goals}</Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={4}>
+                    <Grid.Column><h5>Interceptions:</h5></Grid.Column>
+                    <Grid.Column >{this.state.player.stats.interceptions}</Grid.Column>
+                    <Grid.Column><h5>Passes:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.passes}</Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={4}>
+                    <Grid.Column><h5>Points:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.points}</Grid.Column>
+                    <Grid.Column ><h5>Possession:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.possession_time}</Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={4}>
+                    <Grid.Column><h5>Saves:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.saves}</Grid.Column>
+                    <Grid.Column><h5>Shots:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.shots_taken}</Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={4}>
+                    <Grid.Column><h5>Steals:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.steals}</Grid.Column>
+                    <Grid.Column><h5>Stuns:</h5></Grid.Column>
+                    <Grid.Column>{this.state.player.stats.stuns}</Grid.Column>
+                </Grid.Row>
+            </Grid>
+        )
+    }
+
     render() { 
         
-        if (this.state.loaded){
+        if (this.state.loaded && this.state.isOrange){
             console.log(this.state.mouseX)
             return ( 
-                <div style={{position: 'fixed', left: this.state.mouse.mouseX - 75, bottom: 75}}>
-                   <Card size="large" style={{height:"300px",zIndex:1000}}></Card>
-                </div>
+                   <Card size={"large"} style={{position: 'fixed', left: this.state.mouse.mouseX -75, bottom: 100, width: 400}} >
+                       <Card.Content>
+                            <h1 style={{textAlign:'center'}}>{this.state.player.name}</h1>
+                        {this.buildStats()}
+                   </Card.Content>
+                   </Card>
          );
+        } else if (this.state.loaded && !this.state.isOrange){
+            return (
+                <Card size="large" style={{position: 'fixed', left: this.state.mouse.mouseX - 750, bottom: 75, width: 400}}>
+                    <h1 style={{textAlign:'center'}}>{this.state.player.name}</h1>
+                    {this.buildStats()}
+            </Card>)
+            ;
         } else {
-            return(
-                <div></div>
-            )
+           return ( null);
         }
     }
 }
--- src/components/TeamBox.js
+++ src/components/TeamBox.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Segment, Label, Grid } from 'semantic-ui-react';
-import PlayerLabel from './PlayerLabel';
-import PlayerCard from './PlayerCard';
+import PlayerCard from './PlayerCard'
+import PlayerLabel from './PlayerLabel'
 
 class TeamBox extends Component {
 	constructor(props) {
@@ -28,7 +28,7 @@ class TeamBox extends Component {
 		} else if (this.props.teamData.team === "ORANGE TEAM") {
 			return (
 					<Grid style={{position: "relative", zIndex:0}}>
-						{this.state.renderPlayer ? <PlayerCard player={this.state.playerInfo} mouseInfo={this.state.mouse}/> : null}
+						{this.state.renderPlayer ? <PlayerCard player={this.state.playerInfo} isOrange={true} mouseInfo={this.state.mouse}/> : null}
 						<Segment
 							onMouseEnter={() => {console.log("hovering")}}
 							onMouseLeave={() => this.props.mouseLeave()}
@@ -53,12 +53,13 @@ class TeamBox extends Component {
 					</Grid>
 			 );
 		} return (
-			<Grid>
+			<Grid style={{position: "relative", zIndex:0}}>
+				{this.state.renderPlayer ? <PlayerCard player={this.state.playerInfo} isOrange={false} mouseInfo={this.state.mouse}/> : null}
 				<Segment
 					onMouseLeave={() => this.props.mouseLeave()}
 					size="mini"
 					textAlign='left'
-					style={{height: "150px",width: "15%", marginBottom: "25px", marginLeft: "65%"}}
+					style={{height: "150px",width: "15%", marginBottom: "25px", marginLeft: "65%", position: "relative"}}
 				>
 					{this.props.teamData.players.map((player, index) => (
 						<Grid.Row style={{marginBottom: "20px"}}>
@@ -66,7 +67,9 @@ class TeamBox extends Component {
 								color={"blue"}
 								player={player}
 								key={index}
-								onMouseEnter={(e) => {console.log(e)}}//This is where we render player card, e, being the player object
+								onMouseMove={(e) => {this.setState({mouse:{mouseX: e.screenX, mouseY: e.screenY}})}}
+								onMouseEnter={(player, e) => {this.setState({renderPlayer: true, playerInfo: player, mouse: {mouseX: e.screenX, mouseY: e.screenY}})}}
+								onMouseLeave={() => this.setState({renderPlayer:false})}
 							/>
 						</Grid.Row>
 					))}
