86912a88d548f046861fa2399275b9e99ffea76c
Dowie <dowied4@mymacewan.ca>
2019-11-05 16:58:37 -0700

    Fixes label bug where they would not be aligned. Makes them the same size

--- src/components/TeamBox.js
+++ src/components/TeamBox.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Segment, Label, Grid } from 'semantic-ui-react';
+import { Segment, Label, Grid, Card } from 'semantic-ui-react';
 import PlayerCard from './PlayerCard'
 import PlayerLabel from './PlayerLabel'
+import 'semantic-ui-css/semantic.min.css'
 
 class TeamBox extends Component {
 	constructor(props) {
@@ -27,52 +28,55 @@ class TeamBox extends Component {
 			)
 		} else if (this.props.teamData.team === "ORANGE TEAM") {
 			return (
-					<Grid style={{position: "relative", zIndex:0}}>
-						{this.state.renderPlayer ? <PlayerCard player={this.state.playerInfo} isOrange={true} mouseInfo={this.state.mouse}/> : null}
-						<div
-							onMouseEnter={() => {console.log("hovering")}}
-							onMouseLeave={() => this.props.mouseLeave()}
-							size="mini"
-							style={{width: "15%", marginBottom: "25px", marginLeft: "22%", position: "relative"}}
-						>
-							{this.props.teamData.players.map((player, index) => (
-								<Grid.Row key={index} style={{marginBottom: "20px",textAlign: "center"}}>
-									<PlayerLabel
-										color={"orange"}
-										player={player}
-										key={index}
-										onMouseMove={(e) => {this.setState({mouse:{mouseX: e.pageX, mouseY: e.pageY}})}}
-										onMouseEnter={(player, e) => {this.setState({renderPlayer: true, playerInfo: player, mouse: {mouseX: e.pageX, mouseY: e.pageY}})}}
-										onMouseLeave={() => this.setState({renderPlayer:false})}
-									/>
-								</Grid.Row>
-							))}
-						</div>
-					</Grid>
-			 );
-		} return (
-			<Grid style={{position: "relative", zIndex:0}}>
-				{this.state.renderPlayer ? <PlayerCard player={this.state.playerInfo} isOrange={false} mouseInfo={this.state.mouse}/> : null}
-				<div
-					onMouseLeave={() => this.props.mouseLeave()}
-					size="mini"
-					style={{width: "15%", marginBottom: "25px", marginLeft: "63%", position: "relative"}}
-				>
-					{this.props.teamData.players.map((player, index) => (
-						<Grid.Row key={index} style={{marginBottom: "20px", textAlign: "center"}}>
-							<PlayerLabel
-								color={"blue"}
-								player={player}
-								key={index}
-								onMouseMove={(e) => {this.setState({mouse:{mouseX: e.pageX, mouseY: e.pageY}})}}
-								onMouseEnter={(player, e) => {this.setState({renderPlayer: true, playerInfo: player, mouse: {mouseX: e.pageX, mouseY: e.pageY}})}}
-								onMouseLeave={() => this.setState({renderPlayer:false})}
-							/>
-						</Grid.Row>
-					))}
-				</div>
-			</Grid>
-		 );
+				<Grid style={{position: "relative", zIndex:0}}>
+					{this.state.renderPlayer ? <PlayerCard player={this.state.playerInfo} isOrange={true} mouseInfo={this.state.mouse}/> : null}
+					<div
+						onMouseLeave={() => this.props.mouseLeave()}
+						size="mini"
+						style={{width:"15%",marginBottom: "25px", marginLeft: "22%", position: "relative"}}
+					>
+						{this.props.teamData.players.map((player, index) => {console.log("here");
+						return(
+							<Card key={index} style={{marginBottom: "20px", textAlign: "center"}}>
+								<PlayerLabel
+									color={"orange"}
+									player={player}
+									key={index}
+									onMouseMove={(e) => {this.setState({mouse:{mouseX: e.pageX, mouseY: e.pageY}})}}
+									onMouseEnter={(player, e) => {this.setState({renderPlayer: true, playerInfo: player, mouse: {mouseX: e.pageX, mouseY: e.pageY}})}}
+									onMouseLeave={() => this.setState({renderPlayer:false})}
+								/>
+							</Card>
+						);
+						})}
+					</div>
+				</Grid>
+			);
+		} else {
+			return (
+				<Grid style={{position: "relative", zIndex:0}}>
+					{this.state.renderPlayer ? <PlayerCard player={this.state.playerInfo} isOrange={false} mouseInfo={this.state.mouse}/> : null}
+					<div
+						onMouseLeave={() => this.props.mouseLeave()}
+						size="mini"
+						style={{width: "15%", marginBottom: "25px", marginLeft: "63%", position: "relative"}}
+					>
+						{this.props.teamData.players.map((player, index) => (
+							<Card key={index} style={{marginBottom: "20px", textAlign: "center"}}>
+								<PlayerLabel
+									color={"blue"}
+									player={player}
+									key={index}
+									onMouseMove={(e) => {this.setState({mouse:{mouseX: e.pageX, mouseY: e.pageY}})}}
+									onMouseEnter={(player, e) => {this.setState({renderPlayer: true, playerInfo: player, mouse: {mouseX: e.pageX, mouseY: e.pageY}})}}
+									onMouseLeave={() => this.setState({renderPlayer:false})}
+								/>
+							</Card>
+						))}
+					</div>
+				</Grid>
+			);
+		}
 	}
 }
 
